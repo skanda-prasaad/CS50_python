b0457237e3999363e16dd2e17bdcbfb0e53ec78d
Skanda Prasad <skandaprasad595@gmail.com>
2024-04-09 21:52:33 +0530

    completed

--- week_0/hello.py
+++ week_0/hello.py
@@ -1,1 +1,14 @@
-print("hello,world");
+# Ask user for their name :-
+name = input("what's your name ? ").strip().title()
+# Remove whitespaces from str  and capitalize :-
+# name = name.strip().title()
+
+# Split user's name into firstname and lastname  ;-
+first , last = name.split(" ")
+
+# Say hello to user :-
+print(f"hello, {first}")
+
+
+
+
